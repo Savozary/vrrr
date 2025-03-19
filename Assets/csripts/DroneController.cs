b11a7f5b9be6c7e7dcc5d1ae843e51416834001d
Krch537 <stlkr2028@gmail.com>
2025-03-19 18:12:36 +0300

    ва

--- Assets/csripts/DroneController.cs
+++ Assets/csripts/DroneController.cs
@@ -8,24 +8,22 @@ public class DroneController : MonoBehaviour
 
     void Update()
     {
-        // Получение ввода с контроллера VR
+        // Движение дрона
         float moveHorizontal = Input.GetAxis("Horizontal"); // Левый джойстик
         float moveVertical = Input.GetAxis("Vertical");     // Левый джойстик
-
-        // Движение дрона
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
 
-        // Вращение дрона с помощью правого джойстика
+        // Вращение дрона
         float rotateHorizontal = Input.GetAxis("Rotate"); // Правый джойстик
         transform.Rotate(0, rotateHorizontal * rotationSpeed * Time.deltaTime, 0);
 
-        // Контроль высоты с помощью клавиш A и B
-        if (Input.GetButton("Fire1")) // Кнопка A
+        // Контроль высоты
+        if (Input.GetKey(KeyCode.JoystickButton0)) // Кнопка A
         {
             transform.Translate(0, liftSpeed * Time.deltaTime, 0); // Подъем
         }
-        if (Input.GetButton("Fire2")) // Кнопка B
+        if (Input.GetKey(KeyCode.JoystickButton1)) // Кнопка B
         {
             transform.Translate(0, -liftSpeed * Time.deltaTime, 0); // Спуск
         }
